23798dae8458a61beea7d0efe68498fa13a37300
R41ph21 <ralph.schaur@angeskola.se>
2026-02-10 09:53:15 +0100

    initilize repo

---
+++ WebApp/Pages/Test.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            
+        }
+    }
+}
